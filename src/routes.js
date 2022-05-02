7de515d639cdfa34941ca6a8eafdfd26a7122e85
Vito Breda <vito_odoni@hotmail.com>
2022-05-02 15:43:22 -0300

    chore: Add new routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from './screens/Home';
 import ChecklistDetails from './screens/ChecklistDetails';
 import ChecklistCreate from './screens/ChecklistCreate';
@@ -10,10 +10,10 @@ const Stack = createStackNavigator();
 function Routes() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Checklist Details" component={ChecklistDetails} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Checklist Update" component={ChecklistUpdate} />
       <Stack.Screen name="CheckList Create" component={ChecklistCreate} />
+      <Stack.Screen name="Checklist Details" component={ChecklistDetails} />
     </Stack.Navigator>
   );
 }
