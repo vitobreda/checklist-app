6bc82060bfd000cf985dd70b14d97b58a4485a94
Vito Breda <vito_odoni@hotmail.com>
2022-05-02 15:43:59 -0300

    chore: Prettier fixes

--- src/components/Container/Container.styles.js
+++ src/components/Container/Container.styles.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const Container = styled.View`
-    flex: 1;
-    padding: 16px;
+  flex: 1;
+  padding: 16px;
 `;
 
 export default {
-    Container
+  Container,
 };
--- src/components/ListItem/ListItem.styles.js
+++ src/components/ListItem/ListItem.styles.js
@@ -1,20 +1,20 @@
 import styled from 'styled-components';
 
 const Container = styled.View`
-    flex: 1;
-    padding-bottom: 20px;
+  flex: 1;
+  margin-bottom: 16px;
 `;
 
 const Title = styled.Text`
-    font-size: 20px;
+  font-size: 20px;
 `;
 
 const Description = styled.Text`
-    font-size: 16px;
+  font-size: 16px;
 `;
 
 export default {
-    Container,
-    Title,
-    Description,
-}
+  Container,
+  Title,
+  Description,
+};
